54425081fff6b50f44c4691521f6e14fdd062cd3
Taibu Haroub <haroubtaibu@gmail.com>
2023-02-01 06:05:44 +0300

    Make bucket private

--- warm-up/terraform-and-aws/main.tf
+++ warm-up/terraform-and-aws/main.tf
@@ -17,3 +17,17 @@ provider "aws" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "tyh-app-qa-jr-ty"
 }
+
+# make bucket private
+
+resource "aws_s3_bucket_public_access_block" "s3_block" {
+    # reference to the bucket we just created 
+    # aws_s3_bucket is the type resource
+    # name of the resource from above we used = s3_bucket
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
