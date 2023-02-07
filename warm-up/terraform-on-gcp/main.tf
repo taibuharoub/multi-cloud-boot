01c613bb72fcac9132691c239fcb7a357d2a7ccb
Taibu Haroub <haroubtaibu@gmail.com>
2023-02-07 06:04:40 +0300

    #6 Create config to provision a cloud SQL

--- warm-up/terraform-on-gcp/main.tf
+++ warm-up/terraform-on-gcp/main.tf
@@ -13,7 +13,7 @@ provider "google" {
   project = var.gcp_project_id
 }
 
-# Craete Kubernetes cluster 
+# Craete Kubernetes cluster(GKE) 
 resource "google_container_cluster" "primary" {
   name               = "kubernetes-wramup-cluster-001"
   location           = var.gcp_region
@@ -22,4 +22,22 @@ resource "google_container_cluster" "primary" {
   ip_allocation_policy {
 
   } # terrafprn requires this to create a autopilot cluster
+}
+
+# Create Cloud SQL 
+resource "google_sql_database_instance" "instance" {
+  name             = "cloudwarmupsql"
+  region           = var.gcp_region
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+  deletion_protection = "true"
+}
+
+# Create Database
+resource "google_sql_database" "database" {
+  name = "my-databse"
+  #   instance = "cloudwarmupsql" refer to instance name
+  instance = google_sql_database_instance.instance.name
 }
