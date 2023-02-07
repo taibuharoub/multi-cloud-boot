87adddddcaa38e97e2b2ce9a2babc83723375df4
Taibu Haroub <haroubtaibu@gmail.com>
2023-02-07 05:36:12 +0300

    #6 Create config to provision a kubernetes cluster

--- warm-up/terraform-on-gcp/main.tf
+++ warm-up/terraform-on-gcp/main.tf
@@ -7,3 +7,19 @@ terraform {
     }
   }
 }
+
+# configure the provider 
+provider "google" {
+  project = var.gcp_project_id
+}
+
+# Craete Kubernetes cluster 
+resource "google_container_cluster" "primary" {
+  name               = "kubernetes-wramup-cluster-001"
+  location           = var.gcp_region
+  initial_node_count = 1
+  enable_autopilot   = true
+  ip_allocation_policy {
+
+  } # terrafprn requires this to create a autopilot cluster
+}
